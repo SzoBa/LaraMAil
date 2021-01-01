b365bde89e5ec65d5ae34c62219f3e9c6b6399f4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-01 18:57:24 +0100

    Add post function

--- src/hooks/UsePostData.js
+++ src/hooks/UsePostData.js
@@ -1,29 +1,22 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
 
-export const usePostData = (url, dataArray) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [fetchedData, setFetchedData] = useState([]);
-
+const UsePostData = (url, dataObject, callback) => {
   const options = {
     url: url,
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    data: { content: dataArray },
+    method: "post",
+    data: dataObject,
   };
 
   const postData = async () => {
     const response = await axios(options);
-    setFetchedData(response.data);
-    setIsLoading(false);
+    return response;
   };
 
-  useEffect(() => {
-    postData().catch((error) => console.error(error));
-  }, [options]);
-
-  return [isLoading, fetchedData];
+  postData()
+    .then((result) => {
+      callback(result.data);
+    })
+    .catch((error) => console.error(error));
 };
+
+export default UsePostData;
