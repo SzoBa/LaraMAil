d07678e4a2687c1c6e22eb800679f9f81e18060a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-08 00:46:00 +0100

    Add header options and  messaging to GET function

--- src/hooks/UseGetData.js
+++ src/hooks/UseGetData.js
@@ -1,18 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useGetData = (url) => {
+const useGetData = (url, token, setErrorMessage) => {
   const [isLoading, setIsLoading] = useState(true);
   const [fetchedData, setFetchedData] = useState([]);
 
   useEffect(() => {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
+        Authorization: "Bearer " + token,
+      },
+      url: url,
+    };
     const getData = async () => {
-      const response = await axios(url);
+      const response = await axios(options);
       setFetchedData(response.data);
       setIsLoading(false);
     };
-    getData().catch((error) => console.error(error));
-  }, [url]);
+    getData().catch((error) => {
+      setErrorMessage(error.response.data);
+    });
+  }, [url, token, setErrorMessage]);
 
   return [isLoading, fetchedData];
 };
