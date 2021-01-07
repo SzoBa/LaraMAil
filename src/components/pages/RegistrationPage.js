db81101296869edda73264621443190a13b4257c
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-07 02:12:47 +0100

    Add Registration page logic

--- src/components/pages/RegistrationPage.js
+++ src/components/pages/RegistrationPage.js
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
+import { UserContext } from "../../containers/contexts/UserContext";
 import UsePostData from "../../hooks/UsePostData";
 
 const RegistrationPage = (props) => {
   const history = useHistory();
-  const [registrationError, setRegistrationError] = useState("");
+  const [registrationError, setRegistrationError] = useState([]);
+  const user = useContext(UserContext)[0];
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const userObject = {
-      username: event.target.elements.username.value,
+      name: event.target.elements.username.value,
       email: event.target.elements.email.value,
       password: event.target.elements.password.value,
+      password_confirmation: event.target.elements.password_confirm.value,
     };
-    UsePostData("http://laramail.com/api/register", userObject, (response) => {
-      if (response.status === 201) {
-        setRegistrationError("");
-        history.push("/");
-      } else {
-        setRegistrationError("Error occurred");
+    UsePostData(
+      "http://laramail.com/api/register",
+      user.token,
+      userObject,
+      (response) => {
+        setRegistrationError([]);
+        if (response.status === 201) {
+          history.push("/");
+        }
+        Object.entries(response).forEach(([k, v]) => {
+          v.forEach((value) => {
+            setRegistrationError((old) => [...old, k + ": " + value]);
+          });
+        });
       }
-    });
+    );
   };
 
   return (
@@ -45,9 +56,19 @@ const RegistrationPage = (props) => {
             <input type="text" name="password" />
           </label>
         </div>
+        <div>
+          <label>
+            Confirm password:
+            <input type="text" name="password_confirm" />
+          </label>
+        </div>
         <button type="submit">Register</button>
       </form>
-      <div>{registrationError}</div>
+      <div>
+        {registrationError === null
+          ? ""
+          : registrationError.map((data, index) => <p key={index}>{data}</p>)}
+      </div>
     </div>
   );
 };
