aa7064240e12b9f24eb089820a5516c5282e5e19
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-08 00:44:12 +0100

    Add Compose page

--- src/components/pages/ComposePage.js
+++ src/components/pages/ComposePage.js
@@ -1,47 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
-import UsePutData from "../../hooks/UsePutData";
+import UsePostData from "../../hooks/UsePostData";
+import { UserContext } from "../../containers/contexts/UserContext";
 import MDEditor from "@uiw/react-md-editor";
 
 const ComposePage = (props) => {
   const history = useHistory();
-  const [emailError, setEmailError] = useState("");
+  const user = useContext(UserContext);
+  const [errorMessage, setErrorMessage] = useState([]);
+  const [value, setValue] = useState("**Markdown Syntax!!!**");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const emailObject = {
-      username: event.target.elements.address.value,
-      email: event.target.elements.emailText.value,
+      name: event.target.elements.address.value,
+      subject: event.target.elements.subject.value,
+      message: value,
     };
-    UsePutData("", emailObject, (response) => {
-      if (response.status === 201) {
-        setEmailError("");
-        history.push("/");
-      } else {
-        setEmailError("Error occurred");
-      }
-    });
-  };
 
-  const [value, setValue] = useState("**Hello world!!!**");
+    UsePostData(
+      "http://laramail.com/api/mail",
+      user.token,
+      emailObject,
+      (response) => {
+        setErrorMessage([]);
+        if (response.status === 201) {
+          setErrorMessage(response.data);
+          history.push("/mail/inbox");
+        }
+
+        Object.entries(response).forEach(([k, v]) => {
+          if (v instanceof Array) {
+            v.forEach((value) => {
+              setErrorMessage((old) => [...old, value]);
+            });
+          } else {
+            setErrorMessage((old) => [...old, v]);
+          }
+        });
+      }
+    );
+  };
 
   return (
     <div>
       <h1>This is the e-mail sending page</h1>
       <form method="put" onSubmit={handleSubmit}>
         <div>
-          <label>
-            Send to user:
-            <input type="text" name="address" />
-            <div className="container" name="emailText">
-              <MDEditor value={value} onChange={setValue} />
-              <MDEditor.Markdown source={value} />
-            </div>
-          </label>
+          <div>
+            <label>
+              Send to user:
+              <input type="text" name="address" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Subject:
+              <input type="text" name="subject" />
+            </label>
+          </div>
+          <div className="container" name="emailText">
+            <MDEditor value={value} onChange={setValue} />
+            {/* <MDEditor.Markdown source={value} /> */}
+          </div>
         </div>
         <button type="submit">Send email</button>
       </form>
-      <div>{emailError}</div>
+      <div>
+        {errorMessage === null
+          ? ""
+          : errorMessage.map((data, index) => <p key={index}>{data}</p>)}
+      </div>
     </div>
   );
 };
