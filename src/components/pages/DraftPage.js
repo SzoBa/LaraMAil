da92a1b35a510343cc013a046935e21b19691295
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-08 20:23:58 +0100

    Add draft page

--- src/components/pages/DraftPage.js
+++ src/components/pages/DraftPage.js
@@ -1,55 +1,43 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import UsePutData from "../../hooks/UsePutData";
+import React, { useState, useContext } from "react";
+import UseGetData from "../../hooks/UseGetData";
+import { UserContext } from "../../containers/contexts/UserContext";
 
 const DraftPage = (props) => {
-  const history = useHistory();
+  const user = useContext(UserContext)[0];
   const [errorMessage, setErrorMessage] = useState([]);
-  const [value, setValue] = useState("**Markdown Syntax!!!**");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const emailObject = {
-      name: event.target.elements.address.value,
-      message: value,
-    };
-
-    UsePutData("http://laramail.com/api/login", emailObject, (response) => {
-      if (response.status === 201) {
-        // setErrorMessage([]);
-        //TODO add message
-        history.push("/mail/inbox");
-      }
-      Object.entries(response).forEach(([k, v]) => {
-        v.forEach((value) => {
-          setErrorMessage((old) => [...old, value]);
-        });
-      });
-    });
-  };
+  const mailData = UseGetData(
+    "http://laramail.com/api/mail-draft",
+    user.token,
+    setErrorMessage
+  )[1];
 
   return (
     <div>
-      <h1>This is the e-mail sending page</h1>
-      <form method="put" onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Send to user:
-            <input type="text" name="address" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Text:
-            <textarea type="text" name="emailText"></textarea>
-          </label>
-        </div>
-        <button type="submit">Send email</button>
-      </form>
+      <h1>This is the draft page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Subject</th>
+            <th>Message</th>
+            <th>Sent to</th>
+            <th>Created at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(mailData).map((key, index) => (
+            <tr key={index}>
+              <td>{mailData[key]["subject"]}</td>
+              <td>{mailData[key]["message"]}</td>
+              <td>{mailData[key]["name"]}</td>
+              <td>{mailData[key]["created"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div>
-        {errorMessage === null
-          ? ""
-          : errorMessage.map((data, index) => <p key={index}>{data}</p>)}
+        {Object.keys(errorMessage).map((key, index) => (
+          <p key={index}>{errorMessage[key]}</p>
+        ))}
       </div>
     </div>
   );
