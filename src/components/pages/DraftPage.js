e843ca26f3abfec2b6081fe819fe0b3589c8d3a7
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-09 17:25:16 +0100

    Add save and update functionality

--- src/components/pages/DraftPage.js
+++ src/components/pages/DraftPage.js
@@ -1,8 +1,11 @@
 import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import UseGetData from "../../hooks/UseGetData";
+import UsePutData from "../../hooks/UsePutData";
 import { UserContext } from "../../containers/contexts/UserContext";
 
 const DraftPage = (props) => {
+  const history = useHistory();
   const user = useContext(UserContext)[0];
   const [errorMessage, setErrorMessage] = useState([]);
   const mailData = UseGetData(
@@ -10,6 +13,24 @@ const DraftPage = (props) => {
     user.token,
     setErrorMessage
   )[1];
+
+  const editClickHandler = (event, props) => {
+    UsePutData(
+      `http://laramail.com/api/mail/${props.id}`,
+      user.token,
+      { is_read: true },
+      (response) => {
+        if (response.status === 204) {
+          return history.push(`/mail/edit/${props.id}`);
+        }
+        Object.entries(response).forEach(([k, v]) => {
+          v.forEach((value) => {
+            setErrorMessage((old) => [...old, value]);
+          });
+        });
+      }
+    );
+  };
 
   return (
     <div>
@@ -25,7 +46,7 @@ const DraftPage = (props) => {
         </thead>
         <tbody>
           {Object.keys(mailData).map((key, index) => (
-            <tr key={index}>
+            <tr key={index} onClick={(e) => editClickHandler(e, mailData[key])}>
               <td>{mailData[key]["subject"]}</td>
               <td>{mailData[key]["message"]}</td>
               <td>{mailData[key]["name"]}</td>
