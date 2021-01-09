cae3acbfd223e4801dc9be808b4abbb33462be4d
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-09 14:29:14 +0100

    Add get multiple data function

---
+++ src/hooks/UseGetMultipleData.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useGetMultipleData = (
+  url1,
+  url2,
+  linkerParam,
+  token,
+  setErrorMessage
+) => {
+  const [firstFetchedData, setFirstFetchedData] = useState([]);
+  const [secondFetchedData, setSecondFetchedData] = useState([]);
+
+  useEffect(() => {
+    const options1 = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
+        Authorization: "Bearer " + token,
+      },
+      url: url1,
+    };
+    const options2 = {
+      headers: options1.headers,
+      url: url2,
+    };
+
+    const getData = async () => {
+      const response1 = await axios(options1).then((resp) => {
+        setFirstFetchedData(resp.data);
+        options2.url = url2 + resp.data[linkerParam];
+      });
+      const response2 = await axios(options2).then((resp) =>
+        setSecondFetchedData(resp.data)
+      );
+    };
+    getData().catch((error) => {
+      setErrorMessage(error.response.data);
+    });
+  }, [url1, url2, linkerParam, token, setErrorMessage]);
+
+  return [firstFetchedData, secondFetchedData];
+};
+
+export default useGetMultipleData;
