3def7bbc442e351001af02d2faff65e9b257e389
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-09 01:54:52 +0100

    Add edit page skeleton

--- src/components/pages/EditPage.js
+++ src/components/pages/EditPage.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
+import UseGetData from "../../hooks/UseGetData";
 import UsePutData from "../../hooks/UsePutData";
+import { UserContext } from "../../containers/contexts/UserContext";
 
 const EditPage = (props) => {
   const history = useHistory();
+  const user = useContext(UserContext)[0];
   const [errorMessage, setErrorMessage] = useState([]);
-  const [value, setValue] = useState("**Markdown Syntax!!!**");
+  //const [value, setValue] = useState(""); //this is the message content!!!
+
+  const mailData = UseGetData(
+    `http://laramail.com/api/mail/${props.match.params.id}`,
+    user.token,
+    setErrorMessage
+  )[1];
+
+  // useEffect(() => {
+  //   setMailData(data.message);
+  // }, [data]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const emailObject = {
       name: event.target.elements.address.value,
-      message: value,
+      // message: value,
     };
 
-    //TODO add get data here to fetch the mail before editing
     UsePutData("http://laramail.com/api/login", emailObject, (response) => {
       if (response.status === 201) {
         // setErrorMessage([]);
