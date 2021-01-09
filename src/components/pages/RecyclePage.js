cf043e81b43cf122557ae7fd514e06096cb25d5b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-09 21:37:15 +0100

    Add recycle bin page

---
+++ src/components/pages/RecyclePage.js
@@ -0,0 +1,119 @@
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UseGetData from "../../hooks/UseGetData";
+import UsePutData from "../../hooks/UsePutData";
+import UseDeleteData from "../../hooks/UseDeleteData";
+import { UserContext } from "../../containers/contexts/UserContext";
+
+const RecyclePage = (props) => {
+  const history = useHistory();
+  const user = useContext(UserContext)[0];
+  const [errorMessage, setErrorMessage] = useState([]);
+
+  const mailData = UseGetData(
+    "http://laramail.com/api/mail-recycle",
+    user.token,
+    setErrorMessage
+  )[1];
+
+  const forceDeleteClickHandler = (e, id) => {
+    e.stopPropagation();
+    e.currentTarget.closest("tr").remove();
+
+    UseDeleteData(
+      `http://laramail.com/api/mail/force-delete/${id}`,
+      user.token,
+      (response) => {
+        if (response.status === 204) {
+          return setErrorMessage({ message: "Delete successful!" });
+        }
+        Object.entries(response).forEach(([k, v]) => {
+          v.forEach((value) => {
+            setErrorMessage((old) => [...old, value]);
+          });
+        });
+      }
+    );
+  };
+  const restoreClickHandler = (e, id) => {
+    e.stopPropagation();
+    e.currentTarget.closest("tr").remove();
+    UsePutData(
+      `http://laramail.com/api/mail/${id}`,
+      user.token,
+      { restore: true },
+      (response) => {
+        if (response.status === 204) {
+          return setErrorMessage({ message: "Restore successful!" });
+        }
+        Object.entries(response).forEach(([k, v]) => {
+          v.forEach((value) => {
+            setErrorMessage((old) => [...old, value]);
+          });
+        });
+      }
+    );
+  };
+
+  return (
+    <div>
+      <h1>This is the draft page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Origin</th>
+            <th>Subject</th>
+            <th>Message</th>
+            <th>Sender</th>
+            <th>Sent to</th>
+            <th>Created at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(mailData).map((key, index) => (
+            <tr
+              key={index}
+              onClick={(e) => restoreClickHandler(e, mailData[key])}
+            >
+              <td>
+                {mailData[key]["addressee"] === user
+                  ? "Inbox"
+                  : mailData[key]["sent"]
+                  ? "Sent"
+                  : "Draft"}
+              </td>
+              <td>{mailData[key]["subject"]}</td>
+              <td>{mailData[key]["message"].split(" ")[0]}...</td>
+              <td>{mailData[key]["sender"]}</td>
+              <td>{mailData[key]["addressee"]}</td>
+              <td>{mailData[key]["created"]}</td>
+              <td>
+                <button
+                  type="button"
+                  onClick={(e) =>
+                    forceDeleteClickHandler(e, mailData[key]["id"])
+                  }
+                >
+                  Delete
+                </button>
+                <button
+                  type="button"
+                  onClick={(e) => restoreClickHandler(e, mailData[key]["id"])}
+                >
+                  Restore
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        {Object.keys(errorMessage).map((key, index) => (
+          <p key={index}>{errorMessage[key]}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecyclePage;
