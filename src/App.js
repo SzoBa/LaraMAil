de30b81251bd89d1ebb22b7d58ca2c275d18c7d1
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-10 22:04:52 +0100

    Add styling and background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./style/App.css";
 import { UserContextProvider } from "./containers/contexts/UserContext";
 import { MailContextProvider } from "./containers/contexts/MailContext";
 import { BrowserRouter as Router } from "react-router-dom";
