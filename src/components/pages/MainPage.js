8253e107a4cbd4a8723efc9b454b422a5f3ff40b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-10 23:56:47 +0100

    Add color to text

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const MainPageH3 = styled.h3`
   margin-top: 3%;
   font-size: 5rem;
-  color: red;
-  text-shadow: -2px 0 red, 0 2px red, 2px 0 red, 0 -2px red;
+  color: #d56a6a;
+  text-shadow: -2px 0 #d56a6a, 0 2px #d56a6a, 2px 0 #d56a6a, 0 -2px #d56a6a;
 `;
 
 const MainPageDiv = styled.div`
